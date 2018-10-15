eb9210dc1dfde0b7f5b838612e07afacd8e83bab
AleGareca <gereca20jorge@gmail.com>
2018-10-14 21:23:54 -0300

    inicializando algunas clases

---
+++ tpVinchuca/src/modelado/Persona.java
@@ -0,0 +1,10 @@
+package modelado;
+
+import java.util.ArrayList;
+
+public class Persona {
+	private String nombre;
+	private Integer reviciones;
+	private ArrayList<Muestra> envios;
+	private String nivel;
+}
--- tpVinchuca/src/modelado/Web.java
+++ tpVinchuca/src/modelado/Web.java
@@ -1,5 +1,11 @@
 package modelado;
 
-public class Web {
+import java.util.ArrayList;
 
+public class Web {
+	
+	private ArrayList<Persona> usuarios;
+	private ArrayList<Post> posts;
+	private ArrayList<Organizacion> organizaciones;
 }
+	
