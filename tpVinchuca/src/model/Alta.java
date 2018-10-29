f9d1425803b8b541ff1ca469df00976fc4b58f1a
alex <yimycifuentes17@gmail.com>
2018-10-28 21:14:16 -0300

    primer test de unidad

--- tpVinchuca/src/model/Alta.java
+++ tpVinchuca/src/model/Alta.java
@@ -1,15 +1,11 @@
 package model;
 
 
-
-
 public class Alta implements Nivel{
 
 	@Override
 	public void verificarNivelPara(Muestra muestra) {
 		if(muestra.hayExpertos()) {
-		Verificacion validacionExperto = muestra.buscarVerificacioPor(muestra.aliasDeExperto());	
-		muestra.cambiarTipo(validacionExperto.getTipo());
 			muestra.alta();
 		}
 		
